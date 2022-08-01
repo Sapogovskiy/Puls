2c2c1f0ea49f2927819719f7ccd681ffc55cb0c7
Oleg Sapogovsiy <osapogovskiy@gmail.com>
2022-08-01 19:01:18 +0300

    valid form YES

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,41 @@ $(document).ready(function(){
 
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back');
+
+   //Modal
+
+    $(' [data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function() {
+           $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+           $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+        rules: {
+           name: "required",
+           phone: "required",
+           email: {
+            required: true,
+            email: true
+           }
+        },
+        messages: {
+            name: "Пожалуйста, введите свое имя!",
+            phone: "Введите свой номер телефона!",
+            email: {
+              required: "Введите свой email!",
+              email: "Неправельный адрес почты!"
+            }
+          }
+    });
+    $('#order form').validate();
 });
