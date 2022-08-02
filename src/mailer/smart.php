79433cd167acc3db5ce481f991b238f215bf0ac2
Oleg Sapogovsiy <osapogovskiy@gmail.com>
2022-08-03 02:46:52 +0300

    mailer1.1

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kozakconcert';                 // Наш логин
+$mail->Username = 'kozakconcert@gmail.com';                 // Наш логин
 $mail->Password = 'Sapojo4ek';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('kozakconcert@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('tofid15517@aregods.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
